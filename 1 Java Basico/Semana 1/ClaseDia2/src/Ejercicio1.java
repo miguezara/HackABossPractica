1be41720fc0827eb53ced1fbc59a139e3bfa1326
miguezara <zara_migue@hotmail.com>
2024-02-22 15:34:07 +0100

    ClaseDia2

---
+++ 1 Java Basico/Semana 1/ClaseDia2/src/Ejercicio1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+    public class Ejericio1 {
+        public static void main(String[] args) {
+            // Crear un objeto Scanner
+            Scanner scanner = new Scanner(System.in);
+
+            // Solicitar al usuario que ingrese su edad
+            System.out.println("Ingrese su edad:");
+            int edad = scanner.nextInt();
+
+            // Verificar si la edad es mayor o igual a 18
+            if (edad >= 18) {
+                System.out.println("¡Bienvenido al recital! Puedes ingresar.");
+            } else {
+                System.out.println("Lo siento, eres menor de edad. No puedes ingresar al recital.");
+            }
+
+            // Cerrar el scanner
+            scanner.close();
+        }
+    }
+
+
