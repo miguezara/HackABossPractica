ecbced4ded8d0243555a6e7671925f40d2816a53
miguezara <zara_migue@hotmail.com>
2024-04-06 16:34:23 +0200

    sabado

--- java basico/EjerciciosClase5/src/Ejercicio1.java
+++ java basico/EjerciciosClase5/src/Ejercicio1.java
@@ -1,24 +1,17 @@
-import java.util.Arrays;
-import java.util.List;
-
-
 public class Ejercicio1 {
     public static void main(String[] args) {
+        //Crear un objeto de cada clase hija
+        Mamifero mamifero = new Mamifero(1,"Perro",2,"Perro agua","Pienso",4,"viviparo","negro","campo");
+        Ave ave = new Ave(2, "Águila", 10, "Plumaje", "Carnívoro", 2.5, "Planeo", "Marrón", "Aguileña");
+        Reptil reptil = new Reptil(3, "Serpiente", 3, "Escamas", "Carnívoro", 2.0, "Escamas lisas", true, "Selva");
+    //LLamar a los metodos
+        mamifero.saludar();
+        ave.saludar();
+        reptil.saludar();
+        //Crear un objeto tipo animal
 
-        List<String> nombres = Arrays.asList("Miguel", "Hugo", "Alejandro", "Juan", "Gloria", "Jose Domingo", "Martin", "Carlos", "Manu", "Jorge");
-
-        List<String> lista1 = nombres.stream()
-                .filter(p -> p.startsWith("A"))
-                .map(String::toUpperCase)
-                .toList();
-        lista1.forEach(System.out::println);
-
-        List<String> lista2 = nombres.stream()
-                .filter(p -> p.length() > 5)
-                .map(String::toUpperCase)
-                .toList();
-        lista2.forEach(System.out::println);
-
-
+        //Animal animal = new Animal(4, "Tigre", 7, "Pelaje", "Carnívoro");
+        // animal = mamifero; // Esto dará error
+        //Se intenta asignar un objeto de tipo Mamifero a un objeto de tipo Animal, lo cual no es posible directamente debido a la incompatibilidad de tipos
     }
 }
--- java basico/EjerciciosClase5/src/Ejercicio2.java
+++ java basico/EjerciciosClase5/src/Ejercicio2.java
@@ -1,29 +1,25 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.BinaryOperator;
-
-
 public class Ejercicio2 {
-
     public static void main(String[] args) {
-        List<Integer> numeros = Arrays.asList(5, 12, 8, 15, 3, 20, 10, 25, 6, 18);
-        //Filtrar los numeros mayores a 10
-//forma 1 con reduce
-        BinaryOperator<Integer> binaryOperator = (acum, nro) -> acum + nro;
+        // Crear un vector de tipo Vestimenta
+        Vestimenta[] prendas = new Vestimenta[9];
 
-        Integer suma1 = numeros.stream()
-                .filter(n -> n > 10)
-                .reduce(0, binaryOperator);
-        System.out.println("suma es: " + suma1);
+        // Almacenar 3 zapatos, 3 pantalones, 2 camisetas y 1 sombrero
+        prendas[0] = new Zapato(1, "Zapato1", 50.0, "Nike", "40", "Negro", "Cuero", "Cierre");
+        prendas[1] = new Zapato(2, "Zapato2", 60.0, "Adidas", "42", "Blanco", "Sintético", "Velcro");
+        prendas[2] = new Zapato(3, "Zapato3", 55.0, "Puma", "38", "Rojo", "Tela", "Cordones");
 
+        prendas[3] = new Pantalon(4, "Pantalon1", 45.0, "Levi's", "M", "Azul", "Slim", "Algodón");
+        prendas[4] = new Pantalon(5, "Pantalon2", 40.0, "Diesel", "L", "Negro", "Skinny", "Mezclilla");
+        prendas[5] = new Pantalon(6, "Pantalon3", 50.0, "Zara", "S", "Gris", "Regular", "Poliéster");
 
-        //forma 2 con maptoint y sum
+        prendas[6] = new Camiseta(7, "Camiseta1", 20.0, "Tommy Hilfiger", "M", "Blanco", "Corta", "Redondo");
+        prendas[7] = new Camiseta(8, "Camiseta2", 25.0, "Calvin Klein", "L", "Negro", "Larga", "V");
 
-        Integer suma2 = numeros.stream()
-                .filter(n -> n > 10)
-                .mapToInt(nro -> nro)
-                .sum();
+        prendas[8] = new Sombrero(9, "Sombrero1", 30.0, "Gucci", "M", "Marrón", "Ala ancha", "M");
 
-        System.out.println("Suma es: " + suma2);
+        // Recorrer el vector y ejecutar el método que corresponde en cada posición
+        for (int i = 0; i < prendas.length; i++) {
+            prendas[i].mostrarMensaje();
+        }
     }
 }
